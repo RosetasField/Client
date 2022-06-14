e5a1f475d70fb1512b0181ccefbf1b8a48a48731
Paul COMTE <paul.codepro@gmail.com>
2022-06-14 12:48:36 +0200

    :sparkles: bevy-introduction

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,57 @@
-use bevy::prelude::*;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::{prelude::*, window::PresentMode};
+use bevy_fly_camera::*;
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .run();
+        .insert_resource(WindowDescriptor {
+            title: "bevy-introduction simple window".to_string(),
+            width: 1920.,
+            height: 1080.,
+            present_mode: PresentMode::Immediate,
+            cursor_visible: false,
+            ..default()
+        })
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_plugin(FlyCameraPlugin)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+        material: materials.add(Color::rgb(0.9, 0.0, 0.2).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 3.0 })),
+        material: materials.add(Color::rgb(0.8, 0.6, 0.0).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands
+        .spawn()
+        .insert_bundle(PerspectiveCameraBundle::new_3d())
+        .insert(FlyCamera::default());
 }
